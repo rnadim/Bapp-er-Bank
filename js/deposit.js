a6de6b67cf62274939f699c84afa4d1a2c9e5e5e
rnadim <rmohibur010@mail.com>
2022-08-18 00:59:23 +0600

    js new

--- js/deposit.js
+++ js/deposit.js
@@ -6,5 +6,13 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     // step-2: get the deposit amount from the deposit input field 
     const depositField = document.getElementById('deposit-field');
     const depositAmount = depositField.value;
-    console.log(depositAmount)
+
+    // console.log(depositAmount)
+
+    // step-3: get the current deposit total 
+    // for non-input(element other than input, textarea) use innerText to get the text 
+    const depositTotalElement = document.getElementById('deposit-total');
+    const depositTotal = depositTotalElement.innerText;
+    // console.log(depositTotal);
+    depositTotalElement.innerText = depositAmount;
 })
